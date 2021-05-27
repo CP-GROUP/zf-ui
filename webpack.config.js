a1b1040748b35ce7d5952152b444ddd544108016
裴繁鹏 <peifanpeng@126.com>
2021-05-27 21:03:37 +0800

    fix: build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
-// const HtmlWebpackPlugins = require('html-webpack-plugin');
-// const path = require('path');
+const path = require('path');
 
-// module.exports = function(config){
-
-//     const htmlIndexPlugins =   new HtmlWebpackPlugins({
-//         template: path.resolve(__dirname,'./index.html'),
-//         title: 'test',
-//         chunks: ['chunks', 'site-desktop'],
-//         filename: 'index.html',
-//     })
-//     // 使用项目html模板文件，方便做特殊业务
-//     config.plugins[4] = htmlIndexPlugins;
-
-//     return {
-//         devServer:{
-//             port: 7788
-//         }
-//     }
-// }
+module.exports = function(config){
+    if (config.plugins[4]) {
+        config.plugins[4].options.template = path.resolve(__dirname,'./index.html')
+    }
+    console.log(config.plugins.length,'======');
+    return {
+        devServer:{
+            port: 7788
+        }
+    }
+}
