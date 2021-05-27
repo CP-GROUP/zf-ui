dd23eba16ac4d0412660f30fe69b13ccc304c04f
裴繁鹏 <peifanpeng@126.com>
2021-05-27 21:17:33 +0800

    feat: 添加工程配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = function(config){
     if (config.plugins[4]) {
         config.plugins[4].options.template = path.resolve(__dirname,'./index.html')
     }
-    console.log(config.plugins.length,'======');
     return {
         devServer:{
             port: 7788
